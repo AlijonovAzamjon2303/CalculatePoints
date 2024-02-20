d56c95125020f6c81f8d1a43eb0d9e59b31bca6d
A'zamjon <alijonovrobo@gmail.com>
2024-02-20 19:31:18 +0500

    Birinchi yuklash

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        List<int> khabibsScore = new List<int>{6, 5, 7, 9};
+        List<int> conorsScore = new List<int>{5, 6, 5, 4};
+        var result = CalculatePoints(khabibsScore, conorsScore);
+        Console.WriteLine($"Khabib's point : {result[0]}");
+        Console.WriteLine($"Conors's point : {result[1]}");
+    }
+    public static List<int> CalculatePoints(List<int> first, List<int> second)
+    {
+        List<int> result = new List<int>();
+        int pointsFirst = 0, pointsSecond = 0;
+        for(int i = 0; i < first.Count; i++)
+        {
+            if(first[i] > second[i])
+            {
+                pointsFirst++;
+            }
+            if(first[i] < second[i])
+            {
+                pointsSecond++;
+            }
+        }
+        result.Add(pointsFirst);
+        result.Add(pointsSecond);
+        
+        return result;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# CalculatePoints
-Bu project sizga List va Methodlar bilan ishlashni o'rganishda yordam beradi
+# Bu project List va Methodlar bilan ishlashni o'rganishda yordam beradi.  
+
+Project haqida qisqacha 
+
+![Demo](demo.gif)
